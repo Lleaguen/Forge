f5c3b5bd53c65ea8644f90af51cdd0b516b744ec
Franco Romero <francoromero@MacBook-Air-de-Franco.local>
2026-01-13 21:17:49 -0300

    shared, guards, e interfaces

---
+++ apps/api/src/modules/auth/infrastructure/http/filters/domain-exception.filter.ts
@@ -0,0 +1,21 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { DomainError } from '@/shared/errors/domain.error';
+
+@Catch(DomainError)
+export class DomainExceptionFilter implements ExceptionFilter {
+  catch(exception: DomainError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response.status(HttpStatus.BAD_REQUEST).json({
+      message: exception.message,
+      type: 'DOMAIN_ERROR',
+    });
+  }
+}
